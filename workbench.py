8645513633686991386e83551f3a8e848988fa05
Anthony Lorraine <az6155@gmail.com>
2022-01-30 22:16:31 +0800

    HL7 Parser Updates

--- workbench.py
+++ workbench.py
@@ -1,11 +1,90 @@
-import enum
+from datetime import datetime
 import re
 from os import listdir
 from os.path import join, isfile
 from typing import List, Any
+import sys
+from types import ModuleType, FunctionType
+from gc import get_referents
 
+# Custom objects know their class.
+# Function objects seem to know way too much, including modules.
+# Exclude modules as well.
+# f'{round(getsize(self.hl7_message)/1024/1024, 2)}mb')
+BLACKLIST = type, ModuleType, FunctionType
+
+
+def getsize(obj):
+    """sum size of object & members."""
+    if isinstance(obj, BLACKLIST):
+        raise TypeError('getsize() does not take argument of type: '+ str(type(obj)))
+    seen_ids = set()
+    size = 0
+    objects = [obj]
+    while objects:
+        need_referents = []
+        for obj in objects:
+            if not isinstance(obj, BLACKLIST) and id(obj) not in seen_ids:
+                seen_ids.add(id(obj))
+                size += sys.getsizeof(obj)
+                need_referents.append(obj)
+        objects = get_referents(*need_referents)
+    return size
+
+
+HL7_MESSAGE = {
+    'I': {
+        'ADT': {
+            'A01': 'Admission',
+            'A02': 'Transfer',
+            'A03': 'Discharge',
+            'A05': 'Pre-admission',
+            'A08': 'Visit Update',
+            'A11': 'Cancel Admission',
+            'A12': 'Cancel Transfer',
+            'A13': 'Cancel I/P Discharge',
+            'A14': 'Pre-admission',
+            'A21': 'On-Leave',
+            'A22': 'Return from leave',
+            'A27': 'Cancel Pre-admission',
+            'A28': 'PMI Registration',
+            'A31': 'PMI Update',
+            'A34': 'PMI Merge',
+            'A44': 'Change U/R for O/P visit',
+        },
+        'SIU': {
+            'S12': 'Notification of New Appointment',
+            'S14': 'Notification of Appointment Modification',
+            'S15': 'Notification of Appointment Cancellation',
+        }
+    },
+    'E': {
+        'ADT': {
+            'A03': 'Discharge Emergency Visit',
+            'A04': 'Register Emergency Visit',
+            'A08': 'Emergency Visit Update',
+            'A11': 'Cancel Emergency Visit',
+            'A13': 'Cancel Emergency Visit Discharge',
+            'A44': 'Change U/R for Emergency Visit',
+        }
+    },
+    'O': {
+        'ADT': {
+            'A03': 'Discharge',
+            'A04': 'Register Event (Attendance)',
+            'A05': 'Pre-admit a Patient (Booking)',
+            'A08': 'Update Patient Information (Update Booking/Reschedule)',
+            'A11': 'Cancel Visit (Unattend)',
+            'A13': 'Cancel Discharge',
+            'A38': 'Cancel Pre-admit (Booking)',
+            'A44': 'Change U/R for O/P visit',
+            'A31': 'PMI Update',
+        }
+    },
+
+}
+HL7_MESSAGE['S'] = HL7_MESSAGE['I']
 BRD_FILE_PATH = r'C:\Users\e261712\Desktop\1471683'
-
 SB = '\x0b'
 EB = '\x1c'
 CR = '\x0d'
@@ -79,11 +158,16 @@ class HL7Message:
     def __str__(self):
         return self._raw_message
 
+    def __repr__(self):
+        return self._raw_message
+
     def __getitem__(self, item: Any):
         for segment in self.segments:
-            if str(item).lower() in str(segment.name).lower():
-                return segment
-        return self.segments[item]
+            if str(item).lower() == str(segment.name).lower():
+                if segment:
+                    return segment
+                else:
+                    pass
 
 
 class Segment:
@@ -96,21 +180,28 @@ class Segment:
 
     def _parse_fields(self):
         for index, field in enumerate(self._raw_segment.split(self._separators['FIELD'])):
-            index = str(index)
+            if self.name == 'MSH':
+                index = str(index + 1)
+            else:
+                index = str(index)
             for r_index, repeated_field in enumerate(field.split(self._separators['REPETITION'])):
-                if r_index+1 > 1:
-                    r_index = '.'.join(index+str(r_index+1))
+                if r_index + 1 > 1:
+                    r_index = '.'.join(index + str(r_index + 1))
                 else:
                     r_index = index
                 self.fields.append(Field(raw_field=repeated_field, separators=self._separators, index=r_index))
 
     def __repr__(self):
-        return f'{self.name}'
+        return self._raw_segment
 
     def __getitem__(self, item: Any):
         for field in self.fields:
-            if str(item).lower() in str(field.name).lower():
-                return field
+            if str(item).lower() == str(field.name).lower():
+                if field:
+                    return field
+                else:
+                    pass
+        return self.fields
 
     def __str__(self):
         return self._raw_segment
@@ -130,15 +221,19 @@ class Field:
             self.components.append(Component(raw_component=component, separators=self._separators, index=index))
 
     def __repr__(self):
-        return f'_{self.name}'
+        return self._raw_field
 
     def __str__(self):
         return self._raw_field
 
     def __getitem__(self, item: Any):
         for component in self.components:
-            if str(item).lower() in str(component.name).lower():
-                return component
+            if str(item).lower() == str(component.name).lower():
+                if component:
+                    return component
+                else:
+                    pass
+        return self.components
 
 
 class Component:
@@ -151,19 +246,23 @@ class Component:
 
     def _parse_subcomponents(self):
         for index, subcomponent in enumerate(self._raw_component.split(self._separators['SUBCOMPONENT'])):
-            index = str(index+1)
+            index = str(index + 1)
             self.subcomponents.append(SubComponent(raw_subcomponent=subcomponent, index=index))
 
     def __repr__(self):
-        return f'_{self.name}'
+        return self._raw_component
 
     def __str__(self):
         return self._raw_component
 
     def __getitem__(self, item: Any):
         for subcomponent in self.subcomponents:
-            if str(item).lower() in str(subcomponent.name).lower():
-                return subcomponent
+            if str(item).lower() == str(subcomponent.name).lower():
+                if subcomponent:
+                    return subcomponent
+                else:
+                    pass
+        return self.subcomponents
 
 
 class SubComponent:
@@ -172,12 +271,76 @@ class SubComponent:
         self.name = index
 
     def __repr__(self):
-        return f'_{self.name}'
+        return self._raw_subcomponent
 
     def __str__(self):
         return self._raw_subcomponent
 
 
-# print(HL7Message(HL7_Messages[0]))
-print(HL7Message(HL7_Messages[0])['pid'][3])
+class CommonHL7:
+    def __init__(self, raw_message: str):
+        self.raw = raw_message
+        self.hl7_message = HL7Message(raw_message=raw_message)
+        self.message_id = self.hl7_message['msh'][10]
+        self.message_date_time = datetime.strptime(str(self.hl7_message['msh'][7]), '%Y%m%d%H%M%S')
+        self.event_type = self.hl7_message['msh'][9][2]
+        if self.hl7_message['pid']:
+            self.ur_number = self.hl7_message['pid'][3][1]
+            self.first_name = self.hl7_message['pid'][5][2]
+            self.middle_name = self.hl7_message['pid'][5][3]
+            self.last_name = self.hl7_message['pid'][5][1]
+            self.date_of_birth = datetime.strptime(str(self.hl7_message['pid'][7]), '%Y%m%d')
+        else:
+            self.ur_number = ''
+            self.first_name = ''
+            self.middle_name = ''
+            self.last_name = ''
+            self.date_of_birth = ''
+        if len(str(self.hl7_message['pv1'])) > 10:
+            self.visit = self.hl7_message['pv1'][19]
+            self.admission_type = self.hl7_message['pv1'][2]
+            self.type_description = self._get_type_description()
+            self.ward = self._get_ward()
+            self.bed = self._get_bed()
+        else:
+            self.visit = ''
+            self.admission_type = ''
+            self.type_description = ''
+            self.ward = ''
+            self.bed = ''
+        del self.hl7_message
 
+    def _get_type_description(self):
+        message_type = str(self.hl7_message["msh"][9][1])
+        try:
+            description = HL7_MESSAGE[self.admission_type][message_type][self.event_type]
+        except KeyError:
+            description = ''
+        return description
+
+    def _get_ward(self):
+        if self.admission_type == 'E':
+            ward = 'Emergency'
+        else:
+            ward = self.hl7_message['pv1'][3][2]
+        return ward
+
+    def _get_bed(self):
+        if self.admission_type == 'E':
+            bed = self.hl7_message['pv1'][39][2]
+        else:
+            bed = self.hl7_message['pv1'][3][3]
+        return bed
+
+    def __repr__(self):
+        return self.message_id, self.message_date_time, self.event_type, self.type_description, self.ur_number, \
+               self.first_name, self.middle_name, self.last_name, self.date_of_birth, self.visit, self.admission_type, \
+               self.ward, self.bed, self.raw
+
+    def __str__(self):
+        return str(self.__repr__())
+
+
+for message in HL7_Messages:
+    hl7_message = CommonHL7(message)
+    print(hl7_message)
